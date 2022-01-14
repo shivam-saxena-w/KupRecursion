d6ded9db521de9fa692b09d27f710135a05a401e
Shivam Saxena <shivam.saxena@knoldus.com>
2022-01-14 16:41:08 +0530

    logger updated

--- src/main/scala/Logger.scala
+++ src/main/scala/Logger.scala
@@ -3,6 +3,4 @@ object Logger extends App {
   def log(message: String, level:String = "INFO"): String = {
     s"[$level]: $message"
   }
-  println(log("Hello", "World"))
-  println(log("Hello"))
 }
